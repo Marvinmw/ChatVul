ded4972966af050fce6ef768667aa070ded3af38
Marvinmw <1300623733@qq.com>
2023-04-25 12:57:01 +0800

    fix run script

--- run_ChatVul_detector.sh
+++ run_ChatVul_detector.sh
@@ -2,10 +2,11 @@
 echo "Start Scan Give Files"
 project_folder=$1
 max_len=$2
+output=$3
 if [ -d ${project_folder} ]; then
-    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/solidity_vul/smart-contract-vulnerabilities/vulnerabilities -q concept -l ${max_len}
-    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/solidity_vul/alchemy -l ${max_len}
-    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/solidity_vul/not-so-smart-contracts  -l ${max_len}
+    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/checklist/smart-contract-vulnerabilities/vulnerabilities -q concept -l ${max_len} -o ${output}
+    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/checklist/alchemy -l ${max_len} -o ${output}
+    python chatgpt_vul_dector.py -p ${project_folder} -c datasets/checklist/not-so-smart-contracts  -l ${max_len} -o ${output}
 else
     echo "Folder does not exist"
 fi
